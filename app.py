7f4f1b6d118ea4db443b682ee8bb92c28d752f4f
SafwanSipai <sipaisafwan@gmail.com>
2024-05-18 20:32:05 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import re
+from dotenv import load_dotenv
+import utils
+
+load_dotenv()
+driver = utils.load_driver()
+model = utils.load_model()
+
+st.title("Match Report AI :soccer:")
+st.write("""
+    Get an AI generated match report of any football match with just a few clicks!        
+""")
+st.divider()
+
+url_pattern = re.compile(r"^https://www\.fotmob\.com/matches/.+$")
+
+match_url = st.text_input("Enter [FotMob](https://www.fotmob.com/) URL of the match")
+if st.button("Analyze"):
+    if url_pattern.match(match_url):
+        with st.spinner("Generating..."):
+            stats = utils.get_match_data(match_url, driver)
+            analysis = utils.model_inference(model, str(stats))
+        st.subheader("Analysis")
+        st.write(analysis)
+    else:
+        st.warning("Enter a valid url!")
---
+++ utils.py
@@ -0,0 +1,109 @@
+import re
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+
+def get_match_data(url, driver):
+    driver.get(url + ":tab=stats")
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    data = {}
+
+    data["home_team"] = get_team_data(soup, "home")
+    data["away_team"] = get_team_data(soup, "away")
+
+    return data
+
+
+def get_team_data(soup, side):
+    index = 0 if side == "home" else 1
+    data = {}
+
+    teams = re.findall(r"[A-Za-z ]+(?= vs)|(?<=vs )[A-Za-z ]+", soup.find("h1").text)
+
+    data["name"] = teams[index].strip()
+    data["score"] = (
+        soup.find("span", {"class": re.compile(".*MFHeaderStatusScore.*")})
+        .text.split("-")[index]
+        .strip()
+    )
+    data["possesion"] = soup.find_all(
+        "div", {"class": re.compile(".*PossessionSegment .*")}
+    )[index].text
+
+    data.update(get_stats(soup, side))
+
+    return data
+
+
+def get_stats(soup, side):
+    index = 0 if side == "home" else 1
+    stats = {}
+    for stat in soup.find_all("li", {"class": re.compile(".*Stat.*")}):
+        stat_name = stat.find("span", recursive=False).text
+        stats[stat_name] = stat.find_all("div", recursive=False)[index].text
+    return stats
+
+
+def load_model():
+    load_dotenv()
+    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+    generation_config = {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 64,
+        "max_output_tokens": 8192,
+        "response_mime_type": "text/plain",
+    }
+
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+    ]
+
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-flash-latest",
+        safety_settings=safety_settings,
+        generation_config=generation_config,
+        system_instruction=os.environ["SYSTEM_INSTRUCTION"],
+    )
+
+    return model
+
+
+def model_inference(model, input):
+    chat_session = model.start_chat(history=[])
+    response = chat_session.send_message(input)
+
+    return response.text
+
+
+def load_driver():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(options=options, service=service)
+    return driver
